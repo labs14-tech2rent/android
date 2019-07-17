24b50a300850515f1c3dc732c1c1fa39ff5a663b
Jake Esveld <thejakeesveld@gmail.com>
2019-07-17 09:11:55 -0700

    Display data from backend

--- app/src/main/java/com/labs14tech2rent/tech2rent/MainActivity.kt
+++ app/src/main/java/com/labs14tech2rent/tech2rent/MainActivity.kt
@@ -2,11 +2,41 @@ package com.labs14tech2rent.tech2rent
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONArray
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+
+    val urlString = "https://labstech2rentstaging.herokuapp.com/test"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonGet: Button = button_get
+
+        val client: OkHttpClient = OkHttpClient()
+
+        buttonGet.setOnClickListener(View.OnClickListener {
+
+            Thread(Runnable {
+            val request: Request = Request.Builder().url(urlString).build()
+
+            try{
+                val response = client.newCall(request).execute()
+                val result = response.body?.string()
+                val resultJSON: JSONArray = JSONArray(result)
+                textView.text = resultJSON.get(1).toString()
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+            }).start()
+        })
     }
 }
