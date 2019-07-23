0c49314069090c173778ce9ce86d80b02604351e
Jake Esveld <thejakeesveld@gmail.com>
2019-07-23 15:53:36 -0700

    Create basic boiler plate for navDrawer base activity

--- app/src/main/java/com/labs14tech2rent/tech2rent/Activities/BaseActivity.kt
+++ app/src/main/java/com/labs14tech2rent/tech2rent/Activities/BaseActivity.kt
@@ -1,9 +1,11 @@
 package com.labs14tech2rent.tech2rent.Activities
 
-import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import com.labs14tech2rent.tech2rent.R
 import kotlinx.android.synthetic.main.activity_base.*
 
 class BaseActivity : AppCompatActivity() {
@@ -15,6 +17,18 @@ class BaseActivity : AppCompatActivity() {
         toolbar.title = title
 
         val drawerLayout: DrawerLayout = drawer_layout as DrawerLayout
+        val ActionBarToggle: ActionBarDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(ActionBarToggle)
+        ActionBarToggle.syncState()
 
+        val navView: NavigationView = nav_view
+
+        navView.setNavigationItemSelectedListener {it ->
+            when(it.itemId){
+
+            }
+            drawerLayout.closeDrawers()
+            true
+        }
     }
 }
