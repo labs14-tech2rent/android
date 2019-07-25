42ccfce0a25d370823c8ddaa9386fb05304bcce2
Spencer Stock <spencerstock@gmail.com>
2019-07-25 13:49:23 -0600

    Refactored whole application to androidx

--- app/src/main/java/com/labs14tech2rent/tech2rent/activities/NewListing.kt
+++ app/src/main/java/com/labs14tech2rent/tech2rent/activities/NewListing.kt
@@ -1,7 +1,7 @@
 package com.labs14tech2rent.tech2rent.activities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.labs14tech2rent.tech2rent.R
 
 class NewListing : AppCompatActivity() {
