fd565e712be02284c14a6f5c82843c78cedb807c
Jake Esveld <thejakeesveld@gmail.com>
2019-08-07 10:22:18 -0700

    Set up bindModel function

--- app/src/main/java/com/labs14tech2rent/tech2rent/adapters/DashboardRecyclerAdapterMain.kt
+++ app/src/main/java/com/labs14tech2rent/tech2rent/adapters/DashboardRecyclerAdapterMain.kt
@@ -8,7 +8,12 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.labs14tech2rent.tech2rent.R
 import com.labs14tech2rent.tech2rent.models.Listing
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.listing_single_item.view.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONObject
 
 class DashboardRecyclerAdapterMain(val dataList: List<Listing>): RecyclerView.Adapter<DashboardRecyclerAdapterMain.ViewHolder>() {
     class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
@@ -19,10 +24,43 @@ class DashboardRecyclerAdapterMain(val dataList: List<Listing>): RecyclerView.Ad
         val textListingPrice: TextView = view.single_item_price_textview
         val textListingLocation: TextView = view.single_item_location_textview
         val listingImageProfile: ImageView = view.single_item_profile_pic_imageview
+        val parent: View = view.parent_layout
 
 
         fun bindModel(listing: Listing){
 
+            textListingTitle.text = listing.name
+            textListingDescription.text = listing.description
+            textListingPrice.text = listing.listing_price
+            Thread(Runnable {
+                try{
+                    Picasso.get().load(listing.picture_url).into(listingImagePreview)
+                }catch (e: Exception){}
+
+                val client: OkHttpClient = OkHttpClient()
+                val userid = listing.user_id
+
+                val request: Request = Request.Builder().get()
+                    .url("http://labstech2rentstaging.herokuapp.com/api/users/$userid/reviews")
+                    .addHeader("Content-Type", "application/json;charset=UTF-8")
+                    .build()
+
+                val response: Response = client.newCall(request).execute()
+
+                val JSONstring = response.body()?.string()
+                val profileJSON = JSONObject(JSONstring)
+
+                try {
+                    Picasso.get().load(profileJSON.getString("profile_picture")).into(listingImageProfile)
+                }catch (e: Exception){}
+            }).start()
+
+
+            parent.setOnClickListener(View.OnClickListener {
+                /*
+                * Transfer to item details page
+                * */
+            })
         }
     }
 
