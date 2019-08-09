b88e3398d3f6b0a3fbb2ceda43e65c7674b9e589
Jake Esveld <thejakeesveld@gmail.com>
2019-08-09 09:50:52 -0700

    Start profile fragment Networking logic

--- app/src/main/java/com/labs14tech2rent/tech2rent/Fragments/ViewProfileFragment.kt
+++ app/src/main/java/com/labs14tech2rent/tech2rent/Fragments/ViewProfileFragment.kt
@@ -1,7 +1,6 @@
 package com.labs14tech2rent.tech2rent.Fragments
 
-import android.content.Context
-import android.net.Uri
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -16,6 +15,10 @@ import com.labs14tech2rent.tech2rent.models.User
 import com.squareup.picasso.Picasso
 import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.fragment_view_profile.*
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import org.json.JSONObject
 
 
 class ViewProfileFragment : Fragment() {
@@ -30,6 +33,7 @@ class ViewProfileFragment : Fragment() {
 
     private var listing: Listing? = null
     private var user: User? = null
+    val client = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,16 +43,38 @@ class ViewProfileFragment : Fragment() {
     }
 
     override fun onCreateView(
-
-        /*
-        *
-        * GET USER FROM LISTING
-        *
-        * */
-
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val userid = listing?.user_id
+        Thread(Runnable {
+            val request: Request = Request.Builder().get()
+                .url("http://labstech2rentstaging.herokuapp.com/api/users/$userid/reviews")
+                .addHeader("Content-Type", "application/json;charset=UTF-8")
+                .build()
+
+            val response: Response = client.newCall(request).execute()
+
+            val JSONstring = response.body()?.string()
+
+            val profileJSON = JSONObject(JSONstring)
+            user = User(
+                "unknown",
+                profileJSON.getString("email"),
+                profileJSON.getString("name"),
+                profileJSON.getString("profile_picture"),
+                profileJSON.getString("phone"),
+                profileJSON.getString("date_of_birth"),
+                profileJSON.getString("preferred_payment_type"),
+                profileJSON.getString("street"),
+                profileJSON.getString("city"),
+                profileJSON.getString("state"),
+                profileJSON.getInt("zip_code"),
+                profileJSON.getDouble("average_rating")
+            )
+            }).start()
+
+
         return inflater.inflate(R.layout.fragment_view_profile, container, false)
     }
 
@@ -58,11 +84,24 @@ class ViewProfileFragment : Fragment() {
         /*
         * POPULATE UI WITH USER DATA
         * */
-
         user.let {
-            //Picasso.get().load("").into(profileImage)
 
-            textName.text = user!!.name
+        if (listing?.profileImage == null) {
+            try {
+
+                val temp: Bitmap = Picasso.get().load(user?.profile_picture).get()
+                profileImage.setImageBitmap(temp)
+                listing?.profileImage = temp
+
+            } catch (e: Exception) {
+            }
+        }else {
+            profileImage.setImageBitmap(listing?.profileImage)
+        }
+        textName.text = user?.name
+        textTitle.text
+        textLocation.text
+        textBio.text
 
         }
 
