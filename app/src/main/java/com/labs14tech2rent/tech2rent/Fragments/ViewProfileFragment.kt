1257d8d9778aa92b1b133a8eaac2941448d98999
Jake Esveld <thejakeesveld@gmail.com>
2019-08-09 10:27:44 -0700

    Set population for Profile fragment, catching error for null fields

--- app/src/main/java/com/labs14tech2rent/tech2rent/Fragments/ViewProfileFragment.kt
+++ app/src/main/java/com/labs14tech2rent/tech2rent/Fragments/ViewProfileFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.fragment.app.DialogFragment
 
 import com.labs14tech2rent.tech2rent.R
 import com.labs14tech2rent.tech2rent.models.Listing
@@ -21,15 +22,9 @@ import okhttp3.Response
 import org.json.JSONObject
 
 
-class ViewProfileFragment : Fragment() {
+class ViewProfileFragment : DialogFragment() {
 
-    val profileImage: CircleImageView = image_profile_view
-    val textName: TextView = text_name_view
-    val textTitle: TextView = text_title_view
-    val textLocation: TextView = text_location_view
-    val buttonTechListed: ImageButton = button_view_tech_listed
-    val buttonMessage: ImageButton = button_message
-    val textBio: TextView = text_bio
+
 
     private var listing: Listing? = null
     private var user: User? = null
@@ -46,8 +41,26 @@ class ViewProfileFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        return inflater.inflate(R.layout.fragment_view_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val profileImage: CircleImageView? = image_profile_view
+        val textName: TextView? = text_name_view
+        val textTitle: TextView? = text_title_view
+        val textLocation: TextView? = text_location_view
+        val buttonTechListed: ImageButton? = button_view_tech_listed
+        val buttonMessage: ImageButton? = button_message
+        val textBio: TextView? = text_bio
+
+
         val userid = listing?.user_id
         Thread(Runnable {
+            /*
+            * GET USER REQUEST
+            * */
             val request: Request = Request.Builder().get()
                 .url("http://labstech2rentstaging.herokuapp.com/api/users/$userid/reviews")
                 .addHeader("Content-Type", "application/json;charset=UTF-8")
@@ -58,28 +71,36 @@ class ViewProfileFragment : Fragment() {
             val JSONstring = response.body()?.string()
 
             val profileJSON = JSONObject(JSONstring)
-            user = User(
-                "unknown",
-                profileJSON.getString("email"),
-                profileJSON.getString("name"),
-                profileJSON.getString("profile_picture"),
-                profileJSON.getString("phone"),
-                profileJSON.getString("date_of_birth"),
-                profileJSON.getString("preferred_payment_type"),
-                profileJSON.getString("street"),
-                profileJSON.getString("city"),
-                profileJSON.getString("state"),
-                profileJSON.getInt("zip_code"),
-                profileJSON.getDouble("average_rating")
-            )
-            }).start()
-
-
-        return inflater.inflate(R.layout.fragment_view_profile, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+            try {
+                user = User(
+                    "unknown",
+                    profileJSON.getString("email"),
+                    profileJSON.getString("name"),
+                    profileJSON.getString("profile_picture"),
+                    profileJSON.getString("phone"),
+                    profileJSON.getString("date_of_birth"),
+                    profileJSON.getString("preferred_payment_type"),
+                    profileJSON.getString("street"),
+                    profileJSON.getString("city"),
+                    profileJSON.getString("state"),
+                    profileJSON.getInt("zip_code"),
+                    profileJSON.getDouble("average_rating")
+                )
+            }catch (e: Exception){
+                user = User("unkown",
+                    profileJSON.getString("email"),
+                    profileJSON.getString("name"))
+                try {
+                    user?.profile_picture = profileJSON.getString("profile_picture")
+                }catch (e: Exception){}
+                try {
+                    user?.city = profileJSON.getString("city")
+                }catch (e: Exception){}
+                try {
+                    user?.state = profileJSON.getString("state")
+                }catch (e: Exception){}
+            }
+        }).start()
 
         /*
         * POPULATE UI WITH USER DATA
@@ -90,18 +111,18 @@ class ViewProfileFragment : Fragment() {
             try {
 
                 val temp: Bitmap = Picasso.get().load(user?.profile_picture).get()
-                profileImage.setImageBitmap(temp)
+                profileImage?.setImageBitmap(temp)
                 listing?.profileImage = temp
 
             } catch (e: Exception) {
             }
         }else {
-            profileImage.setImageBitmap(listing?.profileImage)
+            profileImage?.setImageBitmap(listing?.profileImage)
         }
-        textName.text = user?.name
-        textTitle.text
-        textLocation.text
-        textBio.text
+        textName?.text = user?.name
+        textTitle?.text
+        textLocation?.text = "${user?.city}, ${user?.state}"
+        textBio?.text
 
         }
 
