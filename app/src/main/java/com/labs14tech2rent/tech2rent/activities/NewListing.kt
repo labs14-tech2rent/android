7fdc792180e3b8e98f17b1c437da9b55b21e57c9
Jake Esveld <thejakeesveld@gmail.com>
2019-08-26 13:00:07 -0700

    Add app icon

--- app/src/main/java/com/labs14tech2rent/tech2rent/activities/NewListing.kt
+++ app/src/main/java/com/labs14tech2rent/tech2rent/activities/NewListing.kt
@@ -8,18 +8,24 @@ import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Toast
+import com.amazonaws.mobile.client.AWSMobileClient
 import com.labs14tech2rent.tech2rent.R
 import com.labs14tech2rent.tech2rent.models.Listing
 import kotlinx.android.synthetic.main.activity_new_listing.*
-import okhttp3.MediaType
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
 import org.jetbrains.anko.toast
 import org.json.JSONArray
-import okhttp3.MultipartBody
-
-
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver
+import java.io.File.separator
+import android.os.Environment.getExternalStorageDirectory
+import android.R.attr.bitmap
+import android.os.Environment
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState
+import okhttp3.*
+import java.io.*
 
 
 class NewListing : BaseActivity() {
@@ -30,7 +36,7 @@ class NewListing : BaseActivity() {
         val JSON = MediaType.parse("application/json; charset=utf-8")
     }
 
-
+    var imageBitmap: Bitmap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_listing)
@@ -38,6 +44,7 @@ class NewListing : BaseActivity() {
 
 
         val context = this
+        val client = OkHttpClient()
 
         val sharedPrefs: SharedPreferences = getSharedPreferences("acct", Context.MODE_PRIVATE)
         val uuid = sharedPrefs.getString("uuid", "")
@@ -67,19 +74,7 @@ class NewListing : BaseActivity() {
             }
 
 
-            /*val imageProcess = Thread(Runnable {
 
-                val MEDIA_TYPE = if (sourceImageFile.endsWith("png"))
-                    MediaType.parse("image/png")
-                else
-                    MediaType.parse("image/jpeg")
-
-                val requestBody = MultipartBody.Builder()
-                    .setType(MultipartBody.FORM)
-                    .addFormDataPart("uploaded_file", filename, RequestBody.create(MEDIA_TYPE_PNG, sourceFile))
-                    .addFormDataPart("result", "my_image")
-                    .build()
-            })*/
 
 
             val listing = Listing(0,userid,
@@ -97,7 +92,11 @@ class NewListing : BaseActivity() {
 
 
             Thread(Runnable {
-                val client = OkHttpClient()
+
+
+
+
+
                 val body: RequestBody = RequestBody.create(JSON, listing.toString())
                 val request: Request = Request.Builder()
                     .url(urlString)
@@ -130,7 +129,7 @@ class NewListing : BaseActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if(requestCode == 210 && resultCode == Activity.RESULT_OK){
-            val imageBitmap = data?.extras?.get("data") as Bitmap
+            imageBitmap = data?.extras?.get("data") as Bitmap
             new_listing_imageview.setImageBitmap(imageBitmap)
         }
     }
